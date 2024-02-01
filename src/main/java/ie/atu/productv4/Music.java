71c79d86be05e74d33714863f9c03c3befcddb3a
G00413711@atu.ie <G00413711@atu.ie>
2024-02-01 12:14:54 +0000

    Created Music and T class with getters/setters,variables

---
+++ src/main/java/ie/atu/productv4/Music.java
@@ -0,0 +1,35 @@
+package ie.atu.productv4;
+
+public class Music extends Product{
+
+    private String Artist,Label;
+
+    public Music() {
+        super();
+        Artist = "";
+        Label = "";
+        count++;
+    }
+
+    public void setArtist(String Artist) {
+        this.Artist = Artist;
+    }
+
+    public String getArtist() {
+        return Artist;
+    }
+
+    public void setLabel(String Label) {
+        this.Label = Label;
+    }
+
+    public String getLabel() {
+        return Label;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " by " + Artist;
+    }
+}
+
